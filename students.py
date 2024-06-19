2bdae53b69aedd48bd715c9790df5f93b74b4ab8
Бондаренко А.М. <Bondarenko_AM@sfz.com>
2024-06-19 16:52:37 +0300

    change path to students.csv

--- students.py
+++ students.py
@@ -1,8 +1,11 @@
 import pandas as pd
-df = pd.read_csv('c:\_py_projects\students.csv')
+df = pd.read_csv('c:\_py_projects\students\students.csv')
 
 #12 позиция, столбец Рост
 print(df.iloc[[12]]['Growth'])
 
 #ищем 18 с конца запись
 print(df.iloc[len(df)-18,1])
+
+#2 lesson
+
