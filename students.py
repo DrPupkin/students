919cafad25dc013e24b86fd03c0ce11483d3b1b8
Бондаренко А.М. <Bondarenko_AM@sfz.com>
2024-06-21 14:00:45 +0300

    2 lesson

--- students.py
+++ students.py
@@ -8,4 +8,11 @@ print(df.iloc[[12]]['Growth'])
 print(df.iloc[len(df)-18,1])
 
 #2 lesson
-
+print(df.info())
+#средний вес
+print(df['Rock paper scissors'].value_counts())
+print(df['Rock paper scissors'].value_counts().loc['Ножницы'])
+#выбор столбца по индексу
+print(df.iloc[:,26].value_counts())
+#10 по списку
+print(df[['Weight', 'Sex']].iloc[9])
